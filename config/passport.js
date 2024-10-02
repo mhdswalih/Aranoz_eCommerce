e89a8c4468378fb3e84c281847587eb59dc17b91
mhdswalih <ctmswalihmc@gmail.com>
2024-10-02 10:37:42 +0100

    googleauth

--- config/passport.js
+++ config/passport.js
@@ -5,7 +5,7 @@ const User = require('../model/userModel');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/auth/google/callback',
+    callbackURL: 'https://aranoz.online/auth/google/callback',
   },
   async (token, tokenSecret, profile, done) => {
     try {
